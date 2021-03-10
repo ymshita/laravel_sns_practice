a3a4b3bbb6158f783ca437b8761c87e0e7a8de38
Yuya Yamashita <yama.98904@gmail.com>
2021-03-10 20:09:04 +0900

    Update　デフォルトのユーザー登録画面を編集

--- backend/resources/views/auth/register.blade.php
+++ backend/resources/views/auth/register.blade.php
@@ -1,4 +1,54 @@
-<x-guest-layout>
+@extends('app')
+
+@section('title', 'ユーザー登録')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="mx-auto col col-12 col-sm-11 col-md-9 col-lg-7 col-xl-6">
+            <h1 class="text-center"><a class="text-dark" href="/">memo</a></h1>
+            <div class="card mt-3">
+                <div class="card-body text-center">
+                    <h2 class="h3 card-title text-center mt-2">ユーザー登録</h2>
+                    <x-jet-validation-errors class="mb-4" />
+                    <div class="card-text">
+                        <form method="POST" action="{{ route('register') }}">
+                            @csrf
+                            <div class="md-form">
+                                <label for="name">ユーザー名</label>
+                                <input class="form-control" type="text" id="name" name="name" required
+                                    value="{{ old('name') }}">
+                                <small>英数字3〜16文字(登録後の変更はできません)</small>
+                            </div>
+                            <div class="md-form">
+                                <label for="email">メールアドレス</label>
+                                <input class="form-control" type="text" id="email" name="email" required
+                                    value="{{ old('email') }}">
+                            </div>
+                            <div class="md-form">
+                                <label for="password">パスワード</label>
+                                <input class="form-control" type="password" id="password" name="password" required>
+                            </div>
+                            <div class="md-form">
+                                <label for="password_confirmation">パスワード(確認)</label>
+                                <input class="form-control" type="password" id="password_confirmation"
+                                    name="password_confirmation" required>
+                            </div>
+                            <button class="btn btn-block blue-gradient mt-2 mb-2" type="submit">ユーザー登録</button>
+                        </form>
+                        <div class="mt-0">
+                            <a href="{{ route('login') }}" class="card-text">ログインはこちら</a>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+{{-- <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
             <x-jet-authentication-card-logo />
@@ -7,54 +57,61 @@
         <x-jet-validation-errors class="mb-4" />
 
         <form method="POST" action="{{ route('register') }}">
-            @csrf
+@csrf
 
-            <div>
-                <x-jet-label for="name" value="{{ __('Name') }}" />
-                <x-jet-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+<div>
+    <x-jet-label for="name" value="{{ __('Name') }}" />
+    <x-jet-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus
+        autocomplete="name" />
+</div>
+
+<div class="mt-4">
+    <x-jet-label for="email" value="{{ __('Email') }}" />
+    <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
+</div>
+
+<div class="mt-4">
+    <x-jet-label for="password" value="{{ __('Password') }}" />
+    <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required
+        autocomplete="new-password" />
+</div>
+
+<div class="mt-4">
+    <x-jet-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
+    <x-jet-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation"
+        required autocomplete="new-password" />
+</div>
+
+@if (Laravel\Jetstream\Jetstream::hasTermsAndPrivacyPolicyFeature())
+<div class="mt-4">
+    <x-jet-label for="terms">
+        <div class="flex items-center">
+            <x-jet-checkbox name="terms" id="terms" />
+
+            <div class="ml-2">
+                {!! __('I agree to the :terms_of_service and :privacy_policy', [
+                'terms_of_service' => '<a target="_blank" href="'.route('terms.show').'"
+                    class="underline text-sm text-gray-600 hover:text-gray-900">'.__('Terms of
+                    Service').'</a>',
+                'privacy_policy' => '<a target="_blank" href="'.route('policy.show').'"
+                    class="underline text-sm text-gray-600 hover:text-gray-900">'.__('Privacy
+                    Policy').'</a>',
+                ]) !!}
             </div>
+        </div>
+    </x-jet-label>
+</div>
+@endif
 
-            <div class="mt-4">
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
-            </div>
+<div class="flex items-center justify-end mt-4">
+    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
+        {{ __('Already registered?') }}
+    </a>
 
-            <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
-            </div>
-
-            <div class="mt-4">
-                <x-jet-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
-                <x-jet-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
-            </div>
-
-            @if (Laravel\Jetstream\Jetstream::hasTermsAndPrivacyPolicyFeature())
-                <div class="mt-4">
-                    <x-jet-label for="terms">
-                        <div class="flex items-center">
-                            <x-jet-checkbox name="terms" id="terms"/>
-
-                            <div class="ml-2">
-                                {!! __('I agree to the :terms_of_service and :privacy_policy', [
-                                        'terms_of_service' => '<a target="_blank" href="'.route('terms.show').'" class="underline text-sm text-gray-600 hover:text-gray-900">'.__('Terms of Service').'</a>',
-                                        'privacy_policy' => '<a target="_blank" href="'.route('policy.show').'" class="underline text-sm text-gray-600 hover:text-gray-900">'.__('Privacy Policy').'</a>',
-                                ]) !!}
-                            </div>
-                        </div>
-                    </x-jet-label>
-                </div>
-            @endif
-
-            <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
-                </a>
-
-                <x-jet-button class="ml-4">
-                    {{ __('Register') }}
-                </x-jet-button>
-            </div>
-        </form>
-    </x-jet-authentication-card>
-</x-guest-layout>
+    <x-jet-button class="ml-4">
+        {{ __('Register') }}
+    </x-jet-button>
+</div>
+</form>
+</x-jet-authentication-card>
+</x-guest-layout> --}}
