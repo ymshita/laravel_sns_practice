7d736d1d1c9c9e0bc6966c28c995022bd8385536
Yuya Yamashita <yama.98904@gmail.com>
2021-04-18 20:21:49 +0900

    Update 記事 VueコンポーネントをDB情報により表示切り替え

--- backend/resources/js/components/ArticleLike.vue
+++ backend/resources/js/components/ArticleLike.vue
@@ -1,10 +1,23 @@
 <template>
-    <div>
-        <button type="button" class="btn m-0 p-1 shadow-none bg-transparent">
-            <i class="fas fa-heart mr-1"/>
-        </button>
-        10
-    </div>
+  <div>
+    <button type="button" class="btn m-0 p-1 shadow-none bg-transparent">
+      <i class="fas fa-heart mr-1" :class="{ 'red-text': this.isLikedBy }" />
+    </button>
+    10
+  </div>
 </template>
 <script>
+export default {
+  props: {
+    initialIsLikedBy: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      isLikedBy: this.initialIsLikedBy,
+    };
+  },
+};
 </script>
