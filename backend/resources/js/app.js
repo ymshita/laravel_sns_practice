3e5e76b4bd0262da3f7e79a49f54ac8a84bae2bf
Yuya Yamashita <yama.98904@gmail.com>
2021-04-25 09:26:39 +0900

    タグ ライブラリを追加

--- backend/resources/js/app.js
+++ backend/resources/js/app.js
@@ -5,10 +5,12 @@ import './bootstrap'
 import 'alpinejs'
 import Vue from 'vue'
 import ArticleLike from './components/ArticleLike.vue'
+import ArticleTagsInput from './components/ArticleTagsInput.vue'
 
 const app = new Vue({
     el: '#app',
     components: {
         ArticleLike,
+        ArticleTagsInput,
     }
 })
