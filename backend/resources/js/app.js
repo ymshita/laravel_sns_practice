1d75d7986079d3582016a6f544dc5475b979a756
Yuya Yamashita <yama.98904@gmail.com>
2021-05-01 21:25:55 +0900

    Update ユーザー 詳細 フォローボタンを追加

--- backend/resources/js/app.js
+++ backend/resources/js/app.js
@@ -6,11 +6,13 @@ import 'alpinejs'
 import Vue from 'vue'
 import ArticleLike from './components/ArticleLike.vue'
 import ArticleTagsInput from './components/ArticleTagsInput.vue'
+import FollowButton from './components/FollowButton.vue'
 
 const app = new Vue({
     el: '#app',
     components: {
         ArticleLike,
         ArticleTagsInput,
+        FollowButton,
     }
 })
