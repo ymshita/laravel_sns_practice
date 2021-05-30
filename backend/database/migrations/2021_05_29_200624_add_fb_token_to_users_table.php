db79211a73d9ba5efc95cbcd2a2aac365d7ae614
Yuya Yamashita <yama.98904@gmail.com>
2021-05-30 18:59:19 +0900

    Update 2021_05_29_200624_add_fb_token_to_users_table.php

--- backend/database/migrations/2021_05_29_200624_add_fb_token_to_users_table.php
+++ backend/database/migrations/2021_05_29_200624_add_fb_token_to_users_table.php
@@ -14,7 +14,7 @@ class AddFbTokenToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('fb_token');
+            $table->string('fb_token')->nullable();
         });
     }
 
